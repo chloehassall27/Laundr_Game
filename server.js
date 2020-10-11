3a6ad11895eb5e597a4e34ffe831b80447b7f19d
kylehassall27 <kylehassall27@gmail.com>
2020-10-11 17:11:42 -0400

    Updated server to handle png files better

--- server.js
+++ server.js
@@ -9,6 +9,20 @@ const requestListener = function (req, res) {
         res.writeHead(200);
         res.end(indexFile);
     }
+    else if(req.url.indexOf('.png') > 0){
+        res.setHeader("Content-Type", "image/png");
+        console.log("." + req.url)
+        fs.readFile("." + req.url, "utf8")
+            .then(contents => {
+                res.writeHead(200);
+                res.end(contents);
+            })
+            .catch(err => {
+                console.log("error")
+                res.writeHead(404);
+                res.end();
+            });
+    }
     else{
         res.setHeader("Content-Type", "text/javascript");
         console.log("." + req.url)
