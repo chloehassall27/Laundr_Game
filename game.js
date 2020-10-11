f75af1589cbde963094e9f49119336d8a368fb0d
kylehassall27 <kylehassall27@gmail.com>
2020-10-11 17:31:03 -0400

    Switched to live server from my server

--- game.js
+++ game.js
@@ -11,3 +11,6 @@ app.stage.addChild(container);
 
 let text = new PIXI.Text('This is a PixiJS text',{fontFamily : 'Arial', fontSize: 24, fill : 0xff1010, align : 'center'});
 app.stage.addChild(text);
+
+let chara = new PIXI.Sprite.from("sprites/charaSpriteSheet.png");
+app.stage.addChild(chara);
