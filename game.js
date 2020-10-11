9aa0e710ad9c5cabf56c67017a5b77afc3cd6866
kylehassall27 <kylehassall27@gmail.com>
2020-10-11 18:14:02 -0400

    Tried to get animated sprite working

--- game.js
+++ game.js
@@ -12,5 +12,30 @@ app.stage.addChild(container);
 let text = new PIXI.Text('This is a PixiJS text',{fontFamily : 'Arial', fontSize: 24, fill : 0xff1010, align : 'center'});
 app.stage.addChild(text);
 
-let chara = new PIXI.Sprite.from("sprites/charaSpriteSheet.png");
-app.stage.addChild(chara);
+
+// // load the texture we need
+// app.loader.add('charaSheet', 'sprites/charaSpriteSheet.json').load((loader, resources) => {
+//   // let chara = new PIXI.AnimatedSprite(resources.charaSheet.spriteSheet.animations["running_WithSock"]);
+//   let sheet = PIXI.Loader.shared.resources["sprites/spritesheet.json"].spritesheet;
+//   animatedSprite = new PIXI.AnimatedSprite(sheet.animations["running_WithSock"]);
+
+//   // Add the bunny to the scene we are building
+//   app.stage.addChild(chara);
+// });
+
+var loader = new PIXI.Loader()
+    .add("sprites/charaSpriteSheet.json")
+    .load(setup);
+
+function setup() {
+  // get a reference to the sprite sheet we've just loaded:
+  let sheet = loader.resources["sprites/charaSpriteSheet.json"].spriteSheet;
+
+  // create an animated sprite
+  animatedCapguy = new PIXI.AnimatedSprite(sheet.animations["running_WithSock"]);
+
+  // set speed, start playback and add it to the stage
+  animatedCapguy.animationSpeed = 0.167; 
+  animatedCapguy.play();
+  app.stage.addChild(animatedCapguy);
+}
