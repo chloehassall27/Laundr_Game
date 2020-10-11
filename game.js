b1cdf403a84baef43540b630480de6025f236e86
kylehassall27 <kylehassall27@gmail.com>
2020-10-11 17:27:06 -0400

    Revert "Updated server to handle png files better"
    This reverts commit 3a6ad11895eb5e597a4e34ffe831b80447b7f19d.

--- game.js
+++ game.js
@@ -11,8 +11,3 @@ app.stage.addChild(container);
 
 let text = new PIXI.Text('This is a PixiJS text',{fontFamily : 'Arial', fontSize: 24, fill : 0xff1010, align : 'center'});
 app.stage.addChild(text);
-
-let player = new PIXI.Sprite.from("images/gilde.png");
-player.x = 16;
-player.y = app.view.height;
-app.stage.addChild(player);
