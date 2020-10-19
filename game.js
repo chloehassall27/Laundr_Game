c4a7d9b60309884249f8561b43972740ecfad593
kylehassall27 <kylehassall27@gmail.com>
2020-10-19 12:48:56 -0400

    Added game logic and obstacle sheet

--- game.js
+++ game.js
@@ -1,50 +1,67 @@
 const WIDTH = 1000;
 const HEIGHT = WIDTH * 2/3
 
+// === Basic app setup === //
 const app = new PIXI.Application({
   width: WIDTH, height: HEIGHT, backgroundColor: 0xF9F9F9, resolution: window.devicePixelRatio || 1,
 });
-
 document.body.appendChild(app.view);
-
-let chara;
 
 const container = new PIXI.Container();
 app.stage.addChild(container);
 
+app.ticker.add(gameLoop);
+let gameOver = false;
+let loaded = false;
 
 // let text = new PIXI.Text('This is a PixiJS text',{fontFamily : 'Arial', fontSize: 24, fill : 0xff1010, align : 'center'});
 // app.stage.addChild(text);
 
-const spriteSheetName = "sprites/charaSpriteSheet.json"
+// === Sprite setup === //
+let player, obstacle;
+app.loader
+  .add('charaSheet', "sprites/charaSpriteSheet.json")
+  .add('obSheet', "sprites/obstacleSprites.json")
+  .load((loader, resources) => {
+    player = new PIXI.AnimatedSprite(resources.charaSheet.spritesheet.animations["running_WithSock"]);
+    player.height = WIDTH/8;
+    player.width = WIDTH/8;
+    player.x = 200;
+    player.y = HEIGHT-(HEIGHT*.1);
+    player.animationSpeed = .15;
+    player.play()
+    app.stage.addChild(player);
 
-// load the texture we need
-app.loader.add('charaSheet', spriteSheetName).load((loader, resources) => {
+    obstacle = new PIXI.Sprite(resources.obSheet.spritesheet.textures["washer.png"])
+    obstacle.height = WIDTH/8;
+    obstacle.width = WIDTH/8;
+    obstacle.x = 400;
+    obstacle.y = HEIGHT-(HEIGHT*.1);
+    app.stage.addChild(obstacle);
 
-  chara = new PIXI.AnimatedSprite(resources.charaSheet.spritesheet.animations["running_WithSock"]);
-  chara.height = WIDTH/8;
-  chara.width = WIDTH/8;
-  chara.animationSpeed = .15;
-  chara.x = 200;
-  chara.y = HEIGHT-(HEIGHT*.1);
-  chara.play()
-  app.stage.addChild(chara);
+    loaded = true;
+  });
 
-});
+// === Main game loop === //
+function gameLoop() {
+  if (!gameOver && loaded) {
+    // funcions to run every game tick, ex moveSprite();
+  }
+}
 
-// var loader = new PIXI.Loader()
-//     .add(spriteSheetName)
-//     .load(setup);
+// Keypress functions
+window.addEventListener("keydown", keysDown);
+window.addEventListener("keyup", keysUp);
+let keys = {};
+function keysDown(e) {
+  keys[e.keyCode] = true;
+}
 
-// function setup() {
-//   // get a reference to the sprite sheet we've just loaded:
-//   let sheet = loader.resources[spriteSheetName].spriteSheet;
+function keysUp(e) {
+  keys[e.keyCode] = false;
+}
 
-//   // create an animated sprite
-//   animatedSprite = new PIXI.AnimatedSprite(sheet.animations["running_WithSock"]);
-
-//   // set speed, start playback and add it to the stage
-//   animatedSprite.animationSpeed = 0.167; 
-//   animatedSprite.play();
-//   app.stage.addChild(animatedSprite);
-// }
+// Other game functions
+function moveSprite() {
+  // Add movement
+}
