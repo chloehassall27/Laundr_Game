3ea8e349d542b0d18cbd4006039126244116f0db
kylehassall27 <kylehassall27@gmail.com>
2020-10-19 12:28:17 -0400

    Got the animated sprite working

--- game.js
+++ game.js
@@ -1,41 +1,50 @@
+const WIDTH = 1000;
+const HEIGHT = WIDTH * 2/3
+
 const app = new PIXI.Application({
-  width: 800, height: 600, backgroundColor: 0xF9F9F9, resolution: window.devicePixelRatio || 1,
+  width: WIDTH, height: HEIGHT, backgroundColor: 0xF9F9F9, resolution: window.devicePixelRatio || 1,
 });
 
 document.body.appendChild(app.view);
 
+let chara;
 
 const container = new PIXI.Container();
 app.stage.addChild(container);
 
 
-let text = new PIXI.Text('This is a PixiJS text',{fontFamily : 'Arial', fontSize: 24, fill : 0xff1010, align : 'center'});
-app.stage.addChild(text);
+// let text = new PIXI.Text('This is a PixiJS text',{fontFamily : 'Arial', fontSize: 24, fill : 0xff1010, align : 'center'});
+// app.stage.addChild(text);
 
+const spriteSheetName = "sprites/charaSpriteSheet.json"
 
-// // load the texture we need
-// app.loader.add('charaSheet', 'sprites/charaSpriteSheet.json').load((loader, resources) => {
-//   // let chara = new PIXI.AnimatedSprite(resources.charaSheet.spriteSheet.animations["running_WithSock"]);
-//   let sheet = PIXI.Loader.shared.resources["sprites/spritesheet.json"].spritesheet;
+// load the texture we need
+app.loader.add('charaSheet', spriteSheetName).load((loader, resources) => {
+
+  chara = new PIXI.AnimatedSprite(resources.charaSheet.spritesheet.animations["running_WithSock"]);
+  chara.height = WIDTH/8;
+  chara.width = WIDTH/8;
+  chara.animationSpeed = .15;
+  chara.x = 200;
+  chara.y = HEIGHT-(HEIGHT*.1);
+  chara.play()
+  app.stage.addChild(chara);
+
+});
+
+// var loader = new PIXI.Loader()
+//     .add(spriteSheetName)
+//     .load(setup);
+
+// function setup() {
+//   // get a reference to the sprite sheet we've just loaded:
+//   let sheet = loader.resources[spriteSheetName].spriteSheet;
+
+//   // create an animated sprite
 //   animatedSprite = new PIXI.AnimatedSprite(sheet.animations["running_WithSock"]);
 
-//   // Add the bunny to the scene we are building
-//   app.stage.addChild(chara);
-// });
-
-var loader = new PIXI.Loader()
-    .add("sprites/charaSpriteSheet.json")
-    .load(setup);
-
-function setup() {
-  // get a reference to the sprite sheet we've just loaded:
-  let sheet = loader.resources["sprites/charaSpriteSheet.json"].spriteSheet;
-
-  // create an animated sprite
-  animatedCapguy = new PIXI.AnimatedSprite(sheet.animations["running_WithSock"]);
-
-  // set speed, start playback and add it to the stage
-  animatedCapguy.animationSpeed = 0.167; 
-  animatedCapguy.play();
-  app.stage.addChild(animatedCapguy);
-}
+//   // set speed, start playback and add it to the stage
+//   animatedSprite.animationSpeed = 0.167; 
+//   animatedSprite.play();
+//   app.stage.addChild(animatedSprite);
+// }
