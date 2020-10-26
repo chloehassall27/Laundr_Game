d7ce9503ed52816ca52e5833ff70b9d423d31117
Oliver Thomas <thomasmaggie@ufl.edu>
2020-10-26 15:20:37 -0400

    Fixed stutter

--- js/game.js
+++ js/game.js
@@ -1,9 +1,26 @@
+/*
+first let me just mention, i had to change where the washer was spawning in, i brought it up higher on the screen, when i first started live server the washer was way too low, i could only see the top of it peeking out? idk
+
+alright so check out this website for details on setTimeOut which is what i used for interval decrementation:
+https://www.geeksforgeeks.org/how-to-change-the-time-interval-of-setinterval-method-at-runtime-using-javascript/
+
+all the variables relating to the spawn time stuff has been grouped togther under "time variables"
+bug when sprite makes it ~2/3 across screen, it kind of stutters? not very visible if you run as is, but if you let it run for long enough that the interval decreases a bunch (or if you just set the interval small to begin with) you can see it very well.
+
+if you comment out the section labeled "??????" (the part where we check if the sprite needs to be deleted) the stutter goes away, but then like,, nothing gets cleaned up lol
+lmk if you want my help to better understand what i did/help in debugging!! <33
+
+OH ALSO i copied the original stuff into original.js! bc the original code does not have this bug so,,, like even tho i mentioned the one part makes the bug go away if you comment it out, the problem is still probably in the new code not the old stuff LMAO
+*/
+
+
 import Obstacle from "./obstacle.js"
 
 const WIDTH = 1000;
-const HEIGHT = WIDTH * 2/3
-var interval = 500;
+const HEIGHT = WIDTH * 2 / 3
+
 var obstacles = [];
+
 
 // === Basic app setup === //
 const app = new PIXI.Application({
@@ -18,6 +35,13 @@ app.ticker.add(gameLoop);
 let gameOver = false;
 let loaded = false;
 
+//time variables
+let interval = 1500;
+let intDecAmt = 0.9;
+let smallestInt = 400;
+let rangeMin = false;
+let currTime;
+
 // let text = new PIXI.Text('This is a PixiJS text',{fontFamily : 'Arial', fontSize: 24, fill : 0xff1010, align : 'center'});
 // app.stage.addChild(text);
 
@@ -28,53 +52,76 @@ app.loader
   .add('obSheet', "sprites/obstacleSprites.json")
   .load((loader, resources) => {
     player = new PIXI.AnimatedSprite(resources.charaSheet.spritesheet.animations["running_WithSock"]);
-    player.height = WIDTH/8;
-    player.width = WIDTH/8;
+    player.height = WIDTH / 8;
+    player.width = WIDTH / 8;
     player.x = 200;
-    player.y = HEIGHT-(HEIGHT*.1);
+    player.y = HEIGHT - (HEIGHT * .1);
     player.animationSpeed = .15;
     player.play()
     app.stage.addChild(player);
 
     //buildObstacles(app.renderer.height / 2, "washerSprite");
     loaded = true;
-    
+
+    //fire the initial obstacle spawn (which will call all other spawns)
+    spawnObstacle();
   });
 
-  function buildObstacles(posy, spriteName){
-    var obstacle = new Obstacle(app.loader.resources.obSheet.spritesheet.animations[spriteName]);
+function buildObstacles(posy, spriteName) {
+  var obstacle = new Obstacle(app.loader.resources.obSheet.spritesheet.animations[spriteName]);
 
-    obstacle.anchor.set(0.5);
-    obstacle.x = app.renderer.width + 100;
-    obstacle.y = posy;
-    obstacle.animationSpeed = .15;
-    obstacle.play()
+  obstacle.anchor.set(0.5);
+  obstacle.scale.set(0.5);
+  obstacle.x = app.renderer.width;
+  obstacle.y = posy;
+  obstacle.animationSpeed = .125;
+  obstacle.play()
 
-    app.stage.addChild(obstacle);
+  app.stage.addChild(obstacle);
 
-    obstacles.push(obstacle);
+  obstacles.push(obstacle);
 }
 
 // === Main game loop === //
 function gameLoop() {
   if (!gameOver && loaded) {
-    interval--;
-    if(interval === 0){
-      interval = 500;
-      buildObstacles(app.renderer.height - 100, "washerSprite");
-    }
-    for(var i = 0; i < obstacles.length; i++){
+    for (var i = 0; i < obstacles.length; i++) {
       const xBox = obstacles[i].getBounds().x + obstacles[i].getBounds().width;
-      if(xBox < 0){
+      obstacles[i].x -= 1.9;
+      //??????????
+      if (xBox === 0) {
         app.stage.removeChild(obstacles[i]);
-        obstacles.splice(0, 1);
+        obstacles.shift();
       }
-      else{
-        obstacles[i].x -= 3;
-      }
+      //end of ?????
     }
     // funcions to run every game tick, ex moveSprite();
   }
+}
+
+function decreaseInterval() {
+  //intDecAmt is the factor you decrease the increment by
+  interval *= intDecAmt;
+
+  //smallestInt is the smallest the interval will ever get
+  if (interval <= smallestInt) {
+    interval = smallestInt;
+    rangeMin = true;
+  }
+}
+
+function spawnObstacle() {
+  //spawn in the actual obstacle
+  buildObstacles(app.renderer.height / 2, "washerSprite");
+
+  //if the current time is greater than [whatever interval you want] and the min range hasn't been reached, dec interval
+  currTime = performance.now();
+  if ((currTime > (interval * 10)) && !rangeMin) {
+    decreaseInterval();
+  }
+
+  //call the next spawn obstacle, with a delay of interval
+  setTimeout(spawnObstacle, interval);
 }
 
 // Keypress functions
