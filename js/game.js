c0b362a4153a67ad113c5f8d76f91b7e2d613b85
pip <piptastc@gmail.com>
2020-11-22 19:31:17 -0500

    backup for if adblock catches the social buttons

--- js/game.js
+++ js/game.js
@@ -169,7 +169,7 @@ function loadOnce() {
 
       //create player object - handles jumping + ducking
       player = new Player(app, jumpS);
-      player.currSprite.stop();
+      player.currSprite.stop(app);
 
 
       //restart functionality stuff
--- js/socials.js
+++ js/socials.js
@@ -12,11 +12,17 @@ export default class socials {
     // twtDiv;
     // fbDiv;
     // socialsDiv;
+    adBlock = false;
 
     constructor(app) {
         this.app = app;
         //this.setupPixiBtns();
         this.setupHTMLBtns();
+        // this.checkForAdBlock(function (isBlocked) {
+        //     if (isBlocked) this.backup();
+        //     else console.log("not blocked");
+        // });
+        setTimeout(this.checkForBlock2.bind(this), 200);
 
     }
 
@@ -28,10 +34,15 @@ export default class socials {
         // this.app.stage.addChild(this.thumbsupShare);
 
         //html method
-        // this.renderTwt(this.twtDiv); //must be rendered every time to set text to include recent score
-        this.socialsDiv.style.top = '80%';
-        this.socialsDiv.style.left = "50%";
-        this.socialsDiv.style.transform = "translate(-50%, -50%)";
+        if (!this.adBlock) {
+            this.socialsDiv.style.top = '80%';
+            this.socialsDiv.style.left = "50%";
+            this.socialsDiv.style.transform = "translate(-50%, -50%)";
+        } else {
+            this.backupSocialsDiv.style.top = '80%';
+            this.backupSocialsDiv.style.left = "50%";
+            this.backupSocialsDiv.style.transform = "translate(-50%, -50%)";
+        }
     }
 
     resetGame() {
@@ -108,20 +119,94 @@ export default class socials {
 
         this.socialsDiv.style.left = "-999%";
 
-        //this.renderTwt();
+        this.renderTwt();
         //this.endGame();
     }
 
     renderTwt() {
-        this.score = SCORE;
-        this.twtDiv.innerHTML = "";
-        let text = `I just got a score of ` + this.score + ` on @LaundrOfficial's hidden #laundr404game!\n\nThink you can do better? Take it for a spin here:\n`;
-        twttr.widgets.createShareButton('https://www.laundr.io/404', this.twtDiv, {
-            text: text
-        });
+        if (!this.adBlock) {
+            this.score = SCORE;
+            this.twtDiv.innerHTML = "";
+            let text = `I just got a score of ` + this.score + ` on @LaundrOfficial's hidden #laundr404game!\n\nThink you can do better? Take it for a spin here:\n`;
+            twttr.widgets.createShareButton('https://www.laundr.io/404', this.twtDiv, {
+                text: text
+            });
+        } else {
+            this.twtBackupDiv.onclick = function () {
+                let link = "https://twitter.com/share?text=I%20just%20got%20a%20score%20of%20" + SCORE + "%20on%20%40LaundrOfficial%27s%20hidden%20%23laundr404game%21%0A%0AThink%20you%20can%20do%20better%3F%20Take%20it%20for%20a%20spin%20here%3A";
+
+                window.open(link);
+            };
+        }
     }
 
     resetTwtDiv() {
-        this.twtDiv.innerHTML = "";
+        if (!this.adBlock) this.twtDiv.innerHTML = "";
+    }
+
+    checkForAdBlock(callback) {
+        console.log("checking..");
+        let URL = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+        fetch(URL, {
+            method: 'HEAD',
+            mode: 'no-cors'
+        })
+            .then(response => callback(false))
+            .catch(error => callback(true));
+    }
+
+    checkForBlock2() {
+        if (this.twtDiv.innerHTML === "") {
+            this.backup();
+        }
+    }
+
+    backup() {
+        console.log("in backup");
+        this.adBlock = true;
+
+        this.laundrDiv = document.getElementById('laundr-game');
+
+        this.backupSocialsDiv = document.createElement('div');
+        this.backupSocialsDiv.classList.add("backupsocials");
+        this.backupSocialsDiv.style.width = "25%";
+        this.backupSocialsDiv.style.position = "absolute";
+        this.backupSocialsDiv.style.textAlign = "center";
+        this.laundrDiv.appendChild(this.backupSocialsDiv);
+
+
+        this.twtBackupDiv = document.createElement('div');
+        this.twtBackupDiv.innerHTML = "Tweet";
+        this.twtBackupDiv.classList.add("backupbird");
+        this.twtBackupDiv.style.display = "inline-block";
+        this.twtBackupDiv.style.verticalAlign = "top";
+        this.twtBackupDiv.style.minWidth = "40%"
+        this.twtBackupDiv.style.color = "blue";
+        this.twtBackupDiv.style.textDecoration = "underline"
+        this.backupSocialsDiv.appendChild(this.twtBackupDiv);
+        this.twtBackupDiv.style.cursor = 'pointer';
+        this.twtBackupDiv.onclick = function () {
+            let link = "https://twitter.com/share?text=I%20just%20got%20a%20score%20of%20" + SCORE + "%20on%20%40LaundrOfficial%27s%20hidden%20%23laundr404game%21%0A%0AThink%20you%20can%20do%20better%3F%20Take%20it%20for%20a%20spin%20here%3A";
+
+            window.open(link);
+        };
+
+        this.fbBackupDiv = document.createElement('div');
+        this.fbBackupDiv.innerHTML = "Share";
+        this.fbBackupDiv.classList.add("backupbird");
+        this.fbBackupDiv.style.display = "inline-block";
+        this.fbBackupDiv.style.verticalAlign = "top";
+        this.fbBackupDiv.style.minWidth = "40%"
+        this.fbBackupDiv.style.color = "blue";
+        this.fbBackupDiv.style.textDecoration = "underline"
+        this.backupSocialsDiv.appendChild(this.fbBackupDiv);
+        this.fbBackupDiv.style.cursor = 'pointer';
+        this.fbBackupDiv.onclick = function () {
+            let link = "https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.laundr.io%2F404&amp;src=sdkpreparse";
+
+            window.open(link);
+        };
+
+        this.backupSocialsDiv.style.left = "-999%";
     }
 }
