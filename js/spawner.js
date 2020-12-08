60598f24faf2b2e61b7ca4b3c0c1f4cca4bf0a2c
thomasoliver2021 <70597512+thomasoliver2021@users.noreply.github.com>
2020-12-07 20:04:46 -0500

    Fixed iron hitbox (#44)

--- js/spawner.js
+++ js/spawner.js
@@ -58,7 +58,7 @@ export default class Spawner {
             //Calculate hit boxes based on which sprite is spawned
             if (spriteName == "washerSprite") obstacle.hitArea = new PIXI.Rectangle(obstacle.x - (obstacle.width * 0.40), obstacle.y - (obstacle.height * 0.38), obstacle.width * .7, obstacle.height * .53);
             else if (spriteName == "laundrySprite") obstacle.hitArea = new PIXI.Rectangle(obstacle.x - obstacle.width * 0.40, obstacle.y - (obstacle.height * 0.01), obstacle.width * 0.68, obstacle.height * 0.53);
-            else if (spriteName == "ironSprite") obstacle.hitArea = new PIXI.Rectangle(obstacle.x - (obstacle.width * 0.44), obstacle.y - (obstacle.height * 0.29), obstacle.width * 0.8, obstacle.height * 0.2);
+            else if (spriteName == "ironSprite") obstacle.hitArea = new PIXI.Rectangle(obstacle.x - (obstacle.width * 0.44), obstacle.y - (obstacle.height * 0.29), obstacle.width * 0.8, obstacle.height * 0.4);
             else obstacle.hitArea = new PIXI.Rectangle(obstacle.x, obstacle.y, 0, 0);
             //console.log(obstacle.hitArea.y);
 
